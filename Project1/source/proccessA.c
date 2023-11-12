e35e674b80722bd10f7736f257e96323393ad2b1
andrpavlou <100830163+andrpavlou@users.noreply.github.com>
2023-11-12 12:30:47 +0000

    .

--- Project1/source/proccessA.c
+++ Project1/source/proccessA.c
@@ -1,4 +1,5 @@
 #define TEXT_SZ 2048
+#define EXIT_PROGRAM "#BYE#"
 
 
 #include <unistd.h>
@@ -10,11 +11,17 @@
 struct shared_actions{
     char write[TEXT_SZ];
     char read[TEXT_SZ];
+    char exit[5];
 };
+
 
 int main(){
 
-    struct shared_actions* actions;
+    struct shared_actions actions0;
+    struct shared_actions* actions ;
+    actions = &actions0;
+    strcpy(actions->exit, EXIT_PROGRAM);
+
     char buffer[BUFSIZ];
 
     key_t key; 
@@ -36,6 +43,10 @@ int main(){
         fprintf(stderr, "Shmat Failed\n");
         exit(EXIT_FAILURE);
     }
+    printf("Shared memory segment with id %d attached at %p\n", shmid, shared_memory);
+
+
+
 
 
 
